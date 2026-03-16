ce9582910c8b9fd71db5e0b554b9fb9aae36f82b
54774 <54774@etu.he2b.be>
2026-03-16 07:18:42 +0100

    1) favoris css

--- public/components/favorite.js
+++ public/components/favorite.js
@@ -6,10 +6,22 @@ export const FavoriteButton = {
             isLoadingPanier: false
         };
     },
+    // Ajout des propriétés calculées pour l'affichage dynamique
+    computed: {
+        // Nombre total d'articles favoris
+        totalFavoris() {
+            return this.panier.length;
+        },
+        // Liste des 5 premiers titres seulement
+        top5Favoris() {
+            return this.panier.slice(0, 5);
+        }
+    },
     mounted() {
         this.fetchPanier();
     },
     methods: {
+        // ... tes méthodes existantes (fetchPanier, toggleFav, etc.) restent identiques
         fetchPanier() {
             fetch('index.php?page=favorite_fetch&action=get_all')
                 .then(response => response.json())
@@ -20,12 +32,9 @@ export const FavoriteButton = {
         toggleFav(art) {
             const isFav = this.panier.some(f => f.ident_art === art.ident_art);
             const action = isFav ? 'remove' : 'add';
-
             fetch(`index.php?page=favorite_fetch&action=${action}&ident_art=${art.ident_art}`)
                 .then(response => response.json())
-                .then(() => {
-                    this.fetchPanier(); // Rafraîchit le panier après l'action
-                });
+                .then(() => { this.fetchPanier(); });
         },
         isInPanier(id) {
             return this.panier.some(f => f.ident_art === id);
@@ -38,31 +47,27 @@ export const FavoriteButton = {
     template: `
 <div class="favorite-page">
     
-    <div id="panier" class="panier-style">
-        <h2>Votre Liste de favoris</h2>
-        <ul id="panier_contenu">
-            <li v-for="item in panier" :key="item.ident_art">
-                <div>
-                    <span class="read-time-badge">{{ item.readtime_art || 0 }} min</span>
-                    <strong>{{ item.title_art }}</strong>
-                </div>
-                <button @click="toggleFav(item)" class="btn-remove-fav"></button>
+    <div class="favorite-stats-box">
+        <h3>Résumé des favoris</h3>
+        <p>Total : <strong>{{ totalFavoris }} articles</strong></p>
+        <ul v-if="totalFavoris > 0">
+            <li v-for="item in top5Favoris" :key="item.ident_art">
+                {{ item.title_art }} 
+                <button @click="toggleFav(item)" class="btn-remove-fav">X</button>
             </li>
-            <li v-if="panier.length === 0">Votre panier est vide.</li>
-        </ul>
-        
-        <button v-if="panier.length > 0" @click="clearPanier" class="btn-delete">Vider le panier</button>
+        </ul><button v-if="panier.length > 0" @click="clearPanier" class="btn-delete">Vider le panier</button>
+        <p v-else>Aucun favori.</p>
     </div>
+
+   
 
     <section class="favorite">
         <h1>Catalogue</h1>
         <div class="favorite-grid">
-            
             <div v-for="art in articles" :key="art.ident_art" class="article-container">
                 <div class="article-content">
                     <img :src="'./media/' + (art.image_art || 'default.jpg')" :alt="art.title_art">
                     <h3>{{ art.title_art }}</h3>
-                    
                     <button 
                         @click="toggleFav(art)" 
                         :class="['btn-fav', isInPanier(art.ident_art) ? 'active' : '']">
@@ -70,7 +75,6 @@ export const FavoriteButton = {
                     </button>
                 </div>
             </div>
-            
         </div>
     </section>
 </div>
